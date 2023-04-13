54c8124279f4facb6e182e86910a49610976cb1f
qodize <vladislav.gusev.dev@gmail.com>
2023-04-14 01:34:15 +0300

    cloud_firestore dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'core/di/di.dart';
+import 'core/firebase_options.dart';
 import 'core/ui/color_schemes.g.dart';
 import 'view/home_page/home_page.dart';
 
 void main() {
   configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
