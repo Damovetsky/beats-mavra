23312adab52e24ce0b13cee9ba8e5075645cd721
Maksim Shvydkiy <forwork.windows@gmail.com>
2023-04-18 12:52:18 +0300

    service new module

--- lib/data/service/beats_service/beats_service.dart
+++ lib/data/service/beats_service/beats_service.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
+import '../../../core/error/exception.dart';
 import './models/beat_model.dart';
 import './exceptions.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
 
 abstract class BeatsService {
   Future<List<BeatModel>> getBeats({
@@ -18,7 +24,7 @@ abstract class BeatsService {
 
   Future<void> deleteBeat(String beatId);
 
-  Future<List<double>> getGraph();
+  Future<List<double>?> getGraph(File file);
 }
 
 @Injectable(as: BeatsService)
@@ -39,16 +45,16 @@ class BeatsServiceImpl implements BeatsService {
         .then((value) => getBeat(beatModel.beatId))
         .onError(
           (FirebaseException error, stackTrace) =>
-              throw exceptionFactory.generateException(error.code),
-        );
+      throw exceptionFactory.generateException(error.code),
+    );
   }
 
   @override
   Future<void> deleteBeat(String beatId) async {
     return beatsCollection.doc(beatId).delete().then((value) => null).onError(
           (FirebaseException error, stackTrace) =>
-              throw exceptionFactory.generateException(error.code),
-        );
+      throw exceptionFactory.generateException(error.code),
+    );
   }
 
   @override
@@ -57,14 +63,15 @@ class BeatsServiceImpl implements BeatsService {
         .doc(beatId)
         .get()
         .then(
-          (value) => value.exists
-              ? BeatModel.fromJson(value.data()!)
-              : throw exceptionFactory.generateException('not-found'),
-        )
+          (value) =>
+      value.exists
+          ? BeatModel.fromJson(value.data()!)
+          : throw exceptionFactory.generateException('not-found'),
+    )
         .onError(
           (FirebaseException error, stackTrace) =>
-              throw exceptionFactory.generateException(error.code),
-        );
+      throw exceptionFactory.generateException(error.code),
+    );
   }
 
   @override
@@ -75,38 +82,67 @@ class BeatsServiceImpl implements BeatsService {
     final cursor = lastVisible == null
         ? beatsCollection.orderBy('beatId').limit(limit)
         : beatsCollection
-            .orderBy('beatId')
-            .limit(limit)
-            .startAfter([lastVisible.beatId]);
+        .orderBy('beatId')
+        .limit(limit)
+        .startAfter([lastVisible.beatId]);
     return cursor
         .get()
         .then(
-          (snapshots) => snapshots.docs
+          (snapshots) =>
+          snapshots.docs
               .map((element) => BeatModel.fromJson(element.data()))
               .toList(growable: false),
-        )
+    )
         .onError(
           (FirebaseException error, stackTrace) =>
-              throw exceptionFactory.generateException(error.code),
-        );
+      throw exceptionFactory.generateException(error.code),
+    );
   }
 
   @override
   Future<BeatModel> updateBeat(BeatModel beatModel) async {
     return getBeat(beatModel.beatId).then(
-      (value) => beatsCollection
-          .doc(beatModel.beatId)
-          .set(beatModel.toJson())
-          .then((value) => getBeat(beatModel.beatId))
-          .onError(
-            (FirebaseException error, stackTrace) =>
-                throw exceptionFactory.generateException(error.code),
+          (value) =>
+          beatsCollection
+              .doc(beatModel.beatId)
+              .set(beatModel.toJson())
+              .then((value) => getBeat(beatModel.beatId))
+              .onError(
+                (FirebaseException error, stackTrace) =>
+            throw exceptionFactory.generateException(error.code),
           ),
     );
   }
 
   @override
-  Future<List<double>> getGraph() {
-    throw UnimplementedError();
+  Future<List<double>> getGraph(File file) async {
+    final bytes = await file.readAsBytes();
+    final bytesRes = [];
+    final resolution = bytes.length ~/ 300;
+    for (int i = 0; i < bytes.length; ++i) {
+      if (i % resolution == 0) {
+        bytesRes.add(bytes[i]);
+      }
+    }
+
+    final response = await http.post(
+        Uri.parse('http://localhost:8084/music/'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'music': bytesRes.toString(),
+        }),
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final dataNew = data.map((e) => double.parse(e.toString())).toList();
+      return dataNew;
+    }
+    if (response.statusCode == 400) {
+      throw BadRequestException();
+    } else {
+      throw UnknownException();
+    }
   }
 }
